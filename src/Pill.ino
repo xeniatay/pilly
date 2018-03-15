fc5c8ae28b4e164fd76deb6427e153e43040a26e
Bo Zhao <bzhao@lyft.com>
2018-03-15 00:15:07 -0700

    Adding remote reminders

--- src/Pill.ino
+++ src/Pill.ino
@@ -9,51 +9,83 @@
 const int ledPin = D6;
 const int buttonPin = D1;
 const long msPerDay = 60*60*24*1000;
+const long holdThreshold = 3000;
 int sleeps = 0;
 int lastActiveSecond;
 bool alertedToday;
+bool pressedToday;
+String macAddress = "";
+bool blinkLed, ledIsOn;
 
 void setup() {
   alertedToday = false;
   pressedToday = false;
+  blinkLed = false;
+  ledIsOn = false;
   lastActiveSecond = -1;
   // input pulldown to natually assume LOW when switch is not pressed
   pinMode(buttonPin, INPUT_PULLDOWN);
   pinMode(ledPin, OUTPUT);
   pinMode(D7, OUTPUT);
 
+  byte mac[6];
+  WiFi.macAddress(mac);
+  for (int i = 0; i < 5; i++) {
+    macAddress += String(mac[i], HEX) + ":";
+  }
+  macAddress += String(mac[5], HEX);
   // Subscribe only to events from my devices
   Particle.subscribe("button_pressed", onButtonPress, MY_DEVICES);
   Particle.subscribe("start_reminder", onStartReminder, MY_DEVICES);
   Particle.subscribe("start_alert", onStartAlert, MY_DEVICES);
   Particle.subscribe("stop_alert", onStopAlert, MY_DEVICES);
-}
 
-void onButtonpress() {
-  pressedToday = true;
-  ledOff();
-  digitalWrite(D7, HIGH);
-}
-
-void onStartReminder() {
-  // turn off current button
-  // flash the other one
-}
-
-void onStartAlert() {
   ledOn();
 }
 
-void onStopAlert() {
+void onButtonPress(const char *event, const char *data) {
+  pressedToday = true;
+  blinkLed = false;
+  ledOff();
+}
+
+void onStartReminder(const char *event, const char *data) {
+  // turn off current button
+  // flash the other one
+  if (String(data) == macAddress) {
+    ledOff();
+    blinkLed = false;
+  } else {
+    // no need to blink if already pressed today
+    blinkLed = !pressedToday;
+  }
+}
+
+void onStartAlert(const char *event, const char *data) {
+  ledOn();
+}
+
+void onStopAlert(const char *event, const char *data) {
   alertedToday = false;
   pressedToday = false;
+  blinkLed = false;
   ledOff();
 }
 
 void loop() {
   // Check for when the button is pressed down
   if (digitalRead(buttonPin) == HIGH) {
-    Particle.publish("button_pressed", PRIVATE);
+    digitalWrite(D7, HIGH);
+    long startTime = millis();
+    while (digitalRead(buttonPin) == HIGH) {
+      delay(1);
+    }
+    long endTime = millis();
+    if (endTime - startTime > holdThreshold) {
+      Particle.publish("start_reminder", macAddress, PRIVATE);
+    } else {
+      Particle.publish("button_pressed", macAddress, PRIVATE);
+    }
   } else {
     digitalWrite(D7, LOW);
   }
@@ -62,6 +94,13 @@ void loop() {
   int currentSecond = Time.second();
   if (currentSecond != lastActiveSecond) {
     lastActiveSecond = currentSecond;
+    if (blinkLed) {
+      if (ledIsOn) {
+        ledOff();
+      } else {
+        ledOn();
+      }
+    }
 
     // Resync the internal clock once a day
     if (millis() >= msPerDay) {
@@ -89,8 +128,10 @@ void loop() {
 
 void ledOn() {
   digitalWrite(ledPin, HIGH);
+  ledIsOn = true;
 }
 
 void ledOff() {
   digitalWrite(ledPin, LOW);
+  ledIsOn = false;
 }
